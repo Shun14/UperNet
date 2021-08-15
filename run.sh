2417867fec59bf0a20cfef0b8d450f1127d78741
Shun14 <zzsshun13@gmail.com>
2021-08-15 15:40:36 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=3 python train.py --config configs/deeplabv2/deeplabv2_resnet101_os8_cityscapes_1024x512_80k.yml  --num_workers 6 --use_vdl --do_eval --save_interval 500 --save_dir deeplabv2_cityscapes_b6 --batch_size 6 --PID train_edge_zzs
+export CUDA_VISIBLE_DEVICES=0,1 python3 -m paddle.distributed.launch train.py --config configs/upernet/upernet_resnet101_os8_cityscapes_512x1024_40k.yml --num_workers 4 --use_vdl --do_eval --save_interval 500 --save_dir upernet_cityscapes_b8
